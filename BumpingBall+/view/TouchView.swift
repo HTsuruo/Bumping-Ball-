e4b475da41f52755f0e5c4d7ac9047538c9bb37a
Tsuru <Tsuruoka@tsuru.local>
2016-08-21 15:54:40 +0900

    modified goden ball.

---
+++ BumpingBall+/view/TouchView.swift
@@ -0,0 +1,21 @@
+//
+//  TouchView.swift
+//  BumpingBall+
+//
+//  Created by Tsuru on H28/08/21.
+//  Copyright © 平成28年 Hideki Tsuruoka. All rights reserved.
+//
+
+import UIKit
+
+class TouchView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
